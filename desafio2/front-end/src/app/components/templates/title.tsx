adc46f9d7ec0521aa110ad6cf8677499b657cc39
SamuelPrado297 <samueldasilvaprado@gmail.com>
2025-01-20 13:16:09 -0300

    feat: cria títulos como props

---
+++ desafio2/front-end/src/app/components/templates/title.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export interface TitleProps {
+    primary: string;
+    secondary: string;
+}
+
+export default function Title(props: TitleProps) {
+    return (
+        <div className="flex gap-2">
+            <div className="flex flex-col">
+                <Link href="/"><p className="text-3xl text-red-600">&#8592;</p></Link>
+                <h1 className="text-2xl font-black">{props.primary}</h1>
+                <h2 className="text-zinc-400">{props.secondary}</h2>
+            </div>
+        </div>
+    )
+}
