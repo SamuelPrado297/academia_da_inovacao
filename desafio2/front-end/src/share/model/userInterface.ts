21f8551f29333cb8752e532d30ee401873a3ab77
SamuelPrado297 <samueldasilvaprado@gmail.com>
2025-01-20 13:22:32 -0300

    feat: interface dos dados para o formulário

---
+++ desafio2/front-end/src/share/model/userInterface.ts
@@ -0,0 +1,9 @@
+//Modelo de interface do banco de dados user
+export interface User {
+    id: number;
+    name: string;
+    cpf: string;
+    phone: string;
+    email: string;
+    nacionality: string
+}
